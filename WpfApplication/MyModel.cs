9f902af9c048966021f062e387cc5f2f1d85080f
Daniel Parauta <dparauta@o-pitblast.com>
2021-05-24 16:54:09 +0100

    First Commit

---
+++ WpfApplication/MyModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication
+{
+    public class MyModel : INotifyPropertyChanged
+    {
+        private string title;
+        public string Title 
+        { 
+            get => title;
+            set
+            {
+                title = value;
+                RaisePropertyChanged("Title");
+            }
+        }
+
+        private int numberId;
+        public int NumberId
+        {
+            get => numberId;
+            set
+            {
+                numberId = value;
+                RaisePropertyChanged("NumberId");
+            }
+        }
+
+
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfApplication/MyViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication
+{
+    public class MyViewModel : BaseINPC
+    {
+        private ObservableCollection<MyModel> listModels;
+        public ObservableCollection<MyModel> ListModels
+        {
+            get { return listModels; }
+            set 
+            { 
+                listModels = value;
+                RaisePropertyChanged("ListModels");
+            }
+        }
+
+
+
+        private MyModel currentModel;
+        public MyModel CurrentModel
+        {
+            get { return currentModel; }
+            set 
+            {
+                currentModel = value;
+                RaisePropertyChanged("CurrentModel");
+
+            }
+        }
+
+        //Construtor
+        public MyViewModel()
+        {
+
+            ListModels = new ObservableCollection<MyModel>()
+            {
+                new MyModel()
+                {
+                    NumberId=1,
+                    Title ="Title1"
+                },
+                new MyModel()
+                {
+                    NumberId=2,
+                    Title ="Title2"
+                } 
+            };
+
+            CurrentModel = ListModels.First();
+
+
+        }
+
+        public void Message()
+        {
+            MessageBox.Show(CurrentModel.NumberId.ToString());
+        }
+    }
+}
